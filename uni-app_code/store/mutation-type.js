26dba4ab1156f4fe912829431cd63d294f14b4bc
夜雨 <65315790+yeyuone@users.noreply.github.com>
2022-05-13 23:11:55 +0800

    Create mutation-type.js

---
+++ uni-app_code/store/mutation-type.js
@@ -0,0 +1,4 @@
+export const INDEX_ARTLIST_PARAMETER = "INDEX_ARTLIST_PARAMETER"//index页导航  ---->  文章列表页 跳转参数存储本地
+export const SET_CLIENT = "SET_CLIENT"
+export const SET_RECEIVEMESSAGE = "SET_RECEIVEMESSAGE" //收到的消息
+export const SET_RECIVETOPIC = "SET_RECIVETOPIC" //收到的主题
